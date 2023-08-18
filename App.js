a3958acafe7398b5731b476c2459a87a98f7db17
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-08-18 14:48:52 +0330

    02-11-->use the state for store

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  //start for using state
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // function goalInputHandler(enterdText) {
+  //   setEnteredGoal(enterdText);
+  // }
+  const goalInputHandler = (enterdText) => {
+    setEnteredGoal(enterdText);
+  }
+
+  // add to exist goals new one used ... for refrencetype
+  // const addGoalHandler = () => { setCourseGoals([...courseGoals,enteredGoal]) }  ***the code like this has no guranty to sow always the last state on realtime for making sure about that use callback function for it 
+
+  // const addGoalHandler = () => { setCourseGoals(currentGoals => { return [...courseGoals, enteredGoal] }) }
+  //shorter way
+  const addGoalHandler = () => { setCourseGoals(currentGoals => [...courseGoals, enteredGoal]) }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer} >
-        <TextInput placeholder='Course Goal' style={styles.input} />
+        <TextInput placeholder='Course Goal' style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
 
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
 
