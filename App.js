53deb8b3b75249519bfe6e6c881cece44f7e38b0
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-08-19 14:40:25 +0330

    02-13-->style the same part

--- App.js
+++ App.js
@@ -32,9 +32,12 @@ export default function App() {
       //? output the resoult
       //? this is map couse courseGoals is [] Array
       //? each Array item should have unique key -> key={goal} , goal cause still don't have id
+      //* 02-13_End
+      //? styling list items 
       */}
+
       <View>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
       </View>
       {/* //*  */}
     </View>
@@ -49,7 +52,14 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row', justifyContent: "space-between", alignItems: 'center'
   },
-  input: { width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }
+  input: { width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 
 });
 
