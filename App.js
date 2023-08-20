38aa507c6a83aa83378fc160661fca249f658f23
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-08-20 05:40:41 +0330

    02-15-->FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   //start for using state
@@ -18,8 +18,13 @@ export default function App() {
 
   //// const addGoalHandler = () => { setCourseGoals(currentGoals => { return [...courseGoals, enteredGoal] }) }
   //// shorter way
-  const addGoalHandler = () => { setCourseGoals(currentGoals => [...courseGoals, enteredGoal]) }
+  //* 02-15
+  //? This item now cant be just normal text its need to be a object with key and value 
+  //| can use uid exept than key 02-15_05:50
+  //// const addGoalHandler = () => { setCourseGoals(currentGoals => [...courseGoals, enteredGoal]) }
 
+  const addGoalHandler = () => { setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal }]) }
+  //*
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer} >
@@ -35,13 +40,22 @@ export default function App() {
       //* 02-13_End
       //? styling list items 
       //* 02-14_End
-      //? ScrollView for page or part of data (just use it for short data 20< item )
+      //? ScrollView for page or part of data (just use it for short data 20 < item )
       */}
 
-      <ScrollView>
-        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
-      </ScrollView>
+      {/* 
+      //// <ScrollView>
+      ////   {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
+      //// </ScrollView> 
+      */}
       {/* //*  */}
+      {/* 
+      //* 02-15_End
+      //? REPLACE ScrollView with FlatList 
+      */}
+
+      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={itemData => (<View style={styles.listItem}><Text>{itemData.item.value}</Text></View>)} />
+      {/* //* */}
     </View>
   );
 }
